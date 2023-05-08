9fe133b02ffb7ba59540a575a34119b0149b2264
Ruud Peters <ruud_peters96@hotmail.com>
2023-05-08 23:43:39 +0200

    restructured configuration, fixed CI pipeline

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)/"
+
+if [ -n "$2" ]; then
+  ansible-playbook -i $PROJECT_DIR/inv/$1.yml $PROJECT_DIR/src/$2.yml
+  return
+fi
 ansible-playbook -i $PROJECT_DIR/inv/$1.yml $PROJECT_DIR/src/$1.yml
